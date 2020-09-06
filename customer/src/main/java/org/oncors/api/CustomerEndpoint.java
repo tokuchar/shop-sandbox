9191fc16db7fabc7ad3b5d0b7d8a7e94e21cf7a9
Tomek <tomaszkucharski95@gmail.com>
2020-09-07 00:03:49 +0200

    @Trace annotation

--- customer/src/main/java/org/oncors/api/CustomerEndpoint.java
+++ customer/src/main/java/org/oncors/api/CustomerEndpoint.java
@@ -1,61 +1,38 @@
 package org.oncors.api;
 
-import io.opentracing.Scope;
-import io.opentracing.Span;
 import io.opentracing.Tracer;
-import io.opentracing.propagation.Format;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import org.oncors.aop.Trace;
 import org.oncors.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.io.IOException;
-import java.net.URI;
-import java.util.*;
+import java.util.List;
 
 @Slf4j
 @RestController
 @EnableSwagger2
 @RequestMapping("/customers")
 public class CustomerEndpoint {
-    @Autowired
-    Tracer tracer;
     @Value("${auth.service.url}")
     String authUrl;
     @Autowired
     RestTemplate restTemplate;
 
     @GetMapping
+    @Trace
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<Customer>> getCustomers(@RequestHeader HttpHeaders httpHeaders) {
-        try (Scope scope = tracer.buildSpan("get-customer-handler").startActive(true)) {
-            Span span = scope.span();
-
-            Map<String, String> fields = new LinkedHashMap<>();
-            fields.put("message", "this is a log message for name " + "tomek");
-            span.log(fields);
-            span.setTag("cisId", "cisIdBlah2344");
-
-            String hello = makeRequest(httpHeaders);
-            throw new NotImplementedException();
-        }
+        String hello = makeRequest(httpHeaders);
+        throw new NotImplementedException();
     }
 
     private String makeRequest(HttpHeaders httpHeaders) {
-        tracer.inject(
-                tracer.activeSpan().context(),
-                Format.Builtin.HTTP_HEADERS,
-                new RequestBuilderCarrier(httpHeaders));
-
         final HttpEntity<String> entity = new HttpEntity<>(httpHeaders);
         ResponseEntity<String> response = restTemplate.exchange("http://localhost:8082/hello", HttpMethod.GET, entity, String.class);
 
@@ -88,21 +65,4 @@ public class CustomerEndpoint {
         throw new NotImplementedException();
     }
 
-    public class RequestBuilderCarrier implements io.opentracing.propagation.TextMap {
-        private final HttpHeaders httpHeaders;
-
-        RequestBuilderCarrier(HttpHeaders httpHeaders) {
-            this.httpHeaders = httpHeaders;
-        }
-
-        @Override
-        public Iterator<Map.Entry<String, String>> iterator() {
-            throw new UnsupportedOperationException("carrier is write-only");
-        }
-
-        @Override
-        public void put(String key, String value) {
-            httpHeaders.add(key, value);
-        }
-    }
 }
